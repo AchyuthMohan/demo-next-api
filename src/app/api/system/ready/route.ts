817924f8e7cc06aed5703cab30f181793c60dc00
Achyuth Mohan <achyuthmohan1@gmail.com>
2025-07-26 10:36:10 +0530

    initial commit

---
+++ src/app/api/system/ready/route.ts
@@ -0,0 +1,9 @@
+export async function GET(request: Request) {
+    const response = {
+        status: "OK",
+    }
+    return new Response(JSON.stringify(response), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' }
+    });
+  }  
